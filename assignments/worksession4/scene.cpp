d049aa22496d7e29e484158a4b18859cba23edfe
Noah Freedley <42617616+KiritoBro2004@users.noreply.github.com>
2026-02-02 18:24:55 -0500

    work sessiion 4 setup

---
+++ assignments/worksession4/scene.cpp
@@ -0,0 +1,124 @@
+#include "scene.h"
+
+// imgui
+#include "imgui/imgui.h"
+#include "imguizmo/imguizmo.h"
+
+// glm
+#include "glm/glm.hpp"
+#include "glm/gtc/type_ptr.hpp"
+
+// batteries
+#include "batteries/opengl.h"
+
+struct {
+    float shininess = 1.0f;
+    glm::vec3 diffuse = glm::vec3(0, 0, 0);//glm::vec3(0.5, 0, 0.5);
+    glm::vec3 specular = glm::vec3(0, 0, 0);//glm::vec3(0, 0.5, 0.5);
+    glm::vec3 ambience = glm::vec3(0, 0, 0);//glm::vec3(0, 0.5, 0.5);
+} debug;
+
+Scene::Scene()
+{
+    suzanne = std::make_unique<ew::Model>("assets/models/suzanne.obj");
+    blinnphong = std::make_unique<ew::Shader>(
+        "assets/shaders/default.vs",
+        "assets/shaders/blinnphong.fs"
+    );
+
+    light = {
+        .brightness = 1.0f,
+        .color = {1.0f, 0.0f, 1.0f},
+        .position = {2.0f, 2.0f, 2.0f},
+    };
+}
+
+Scene::~Scene()
+{
+}
+
+void Scene::Update(float dt)
+{
+    batteries::Scene::Update(dt);
+
+    /* body */
+}
+
+glm::vec3 light_color = glm::vec3(1.0f);
+void Scene::Render(void)
+{
+    const auto view_proj = camera.Projection() * camera.View();
+
+    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    glEnable(GL_CULL_FACE);
+    glCullFace(GL_BACK);
+    glEnable(GL_DEPTH_TEST);
+    // glDisable(GL_DEPTH_TEST);
+
+    blinnphong->use();
+
+    // scene matrices
+    blinnphong->setMat4("model", glm::mat4(1.0f));
+    blinnphong->setMat4("view_proj", view_proj);
+
+    blinnphong->setVec3("camera", camera.position);
+
+    blinnphong->setFloat("material.shininess", debug.shininess);
+    blinnphong->setVec3("material.ambient", debug.ambience);
+    blinnphong->setVec3("material.diffuse", debug.diffuse);
+    blinnphong->setVec3("material.specular", debug.specular);
+    
+    blinnphong->setVec3("light.position", light.position);
+    blinnphong->setVec3("light.color", light_color);
+
+
+    // draw suzanne
+    suzanne->draw();
+}
+
+void Scene::Debug(void)
+{
+    ImGuizmo::BeginFrame();
+    ImGuizmo::SetDrawlist(ImGui::GetBackgroundDrawList());
+    ImGuizmo::SetRect(0, 0, ImGui::GetIO().DisplaySize.x, ImGui::GetIO().DisplaySize.y);
+
+    glm::mat4 m{1.0f};
+    auto *view = glm::value_ptr(camera.View());
+    auto *proj = glm::value_ptr(camera.Projection());
+    
+    ImGuizmo::DrawGrid(view, proj, glm::value_ptr(m), 100.0f);
+
+    auto matrix = glm::translate(glm::mat4(1.0f), light.position);
+
+    ImGuizmo::Manipulate(
+        view,
+        proj,
+        ImGuizmo::TRANSLATE,
+        ImGuizmo::WORLD,
+        glm::value_ptr(matrix)
+    );
+
+    if(ImGuizmo::IsUsing())
+    {
+        light.position = glm::vec3(matrix[3]);
+    }
+
+    cameracontroller.Debug();
+
+    ImGui::Begin("Controlls", nullptr, ImGuiWindowFlags_AlwaysAutoResize);
+
+    ImGui::Checkbox("Paused", &time.paused);
+    ImGui::SliderFloat("Time Factor", &time.factor, 0.0f, 10.0f);
+    ImGui::SliderFloat("Alpha", &debug.shininess, 1.0f, 128.0f);
+    ImGui::ColorEdit3("Light color", &light_color.x);
+    ImGui::ColorEdit3("Ambience color", &debug.ambience.x);
+    ImGui::ColorEdit3("Diffuse color", &debug.diffuse.x);
+    ImGui::ColorEdit3("Specular color", &debug.specular.x);
+
+
+    /* build debug ui here */
+
+    ImGui::End();
+}
